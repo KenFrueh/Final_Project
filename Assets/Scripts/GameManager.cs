d6fe06a0fe7e04575947afe151408b29d06daa33
Kenny  Frueh <kenfrueh@uat.edu>
2020-04-05 16:27:12 -0700

    Created a main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int PlayerLives = 3;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this; //Shows THIS of class in int variable
+            DontDestroyOnLoad(gameObject);
+
+        }
+        else
+        {
+            Destroy(this.gameObject); //Destroy Game manager attached to component
+            Debug.LogError("Error: Tried to make second Game Manager");
+        }
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+        Debug.Log("Hit Play!");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("You Quit...");
+    }
+}
