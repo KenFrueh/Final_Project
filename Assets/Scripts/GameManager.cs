9c61e2be2a209b509a1a10cc7d047780ba651ec4
Kenny  Frueh <kenfrueh@uat.edu>
2020-04-09 20:39:09 -0700

    Added audio and sprites

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public AudioClip Click;
 
     public int PlayerLives = 3;
 
@@ -36,10 +37,12 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(sceneBuildIndex: 1);
         Debug.Log("Hit Play!");
+        AudioSource.PlayClipAtPoint(Click, transform.position);
     }
     public void QuitGame()
     {
         Application.Quit();
         Debug.Log("You Quit...");
+        AudioSource.PlayClipAtPoint(Click, transform.position);
     }
 }
